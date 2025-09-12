973ee2816c30a361f624b3f94cbb27af7f629c20
Arlo <s2533008@kpuenta.ca>
2025-09-11 21:53:14 -0700

    Aligned text

--- GD12_1133_Lab2_Peskoff_Rob/Program.cs
+++ GD12_1133_Lab2_Peskoff_Rob/Program.cs
@@ -16,16 +16,16 @@ namespace GD12_1133_Lab2_Peskoff_Rob
                 }
             */
 
-            Console.WriteLine("Decimal: " + 0 + " -> Binary: " + Convert.ToString(0, 2) + " -> Hex: " + Convert.ToString(0, 16));
-            Console.WriteLine("Decimal: " + 1 + " -> Binary: " + Convert.ToString(1, 2) + " -> Hex: " + Convert.ToString(1, 16));
-            Console.WriteLine("Decimal: " + 2 + " -> Binary: " + Convert.ToString(2, 2) + " -> Hex: " + Convert.ToString(2, 16));
-            Console.WriteLine("Decimal: " + 4 + " -> Binary: " + Convert.ToString(4, 2) + " -> Hex: " + Convert.ToString(4, 16));
-            Console.WriteLine("Decimal: " + 8 + " -> Binary: " + Convert.ToString(8, 2) + " -> Hex: " + Convert.ToString(8, 16));
-            Console.WriteLine("Decimal: " + 16 + " -> Binary: " + Convert.ToString(16, 2) + " -> Hex: " + Convert.ToString(16, 16));
-            Console.WriteLine("Decimal: " + 32 + " -> Binary: " + Convert.ToString(32, 2) + " -> Hex: " + Convert.ToString(32, 16));
-            Console.WriteLine("Decimal: " + 64 + " -> Binary: " + Convert.ToString(64, 2) + " -> Hex: " + Convert.ToString(64, 16));
-            Console.WriteLine("Decimal: " + 100 + " -> Binary: " + Convert.ToString(100, 2) + " -> Hex: " + Convert.ToString(100, 16));
-            Console.WriteLine("Decimal: " + 255 + " -> Binary: " + Convert.ToString(255, 2) + " -> Hex: " + Convert.ToString(255, 16));
+            Console.WriteLine("Decimal: " + 0 + "\t->\tBinary: " + Convert.ToString(0, 2) + "\t\t->\tHex: " + Convert.ToString(0, 16));
+            Console.WriteLine("Decimal: " + 1 + "\t->\tBinary: " + Convert.ToString(1, 2) + "\t\t->\tHex: " + Convert.ToString(1, 16));
+            Console.WriteLine("Decimal: " + 2 + "\t->\tBinary: " + Convert.ToString(2, 2) + "\t\t->\tHex: " + Convert.ToString(2, 16));
+            Console.WriteLine("Decimal: " + 4 + "\t->\tBinary: " + Convert.ToString(4, 2) + "\t\t->\tHex: " + Convert.ToString(4, 16));
+            Console.WriteLine("Decimal: " + 8 + "\t->\tBinary: " + Convert.ToString(8, 2) + "\t\t->\tHex: " + Convert.ToString(8, 16));
+            Console.WriteLine("Decimal: " + 16 + "\t->\tBinary: " + Convert.ToString(16, 2) + "\t\t->\tHex: " + Convert.ToString(16, 16));
+            Console.WriteLine("Decimal: " + 32 + "\t->\tBinary: " + Convert.ToString(32, 2) + "\t\t->\tHex: " + Convert.ToString(32, 16));
+            Console.WriteLine("Decimal: " + 64 + "\t->\tBinary: " + Convert.ToString(64, 2) + "\t\t->\tHex: " + Convert.ToString(64, 16));
+            Console.WriteLine("Decimal: " + 100 + "\t->\tBinary: " + Convert.ToString(100, 2) + "\t\t->\tHex: " + Convert.ToString(100, 16));
+            Console.WriteLine("Decimal: " + 255 + "\t->\tBinary: " + Convert.ToString(255, 2) + "\t->\tHex: " + Convert.ToString(255, 16));
 
             Console.WriteLine("goodbye\n-rob peskoff");
         }
